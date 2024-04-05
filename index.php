8e9c5ed23264214239d4fa99a4eba9103671ba47
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-05 14:23:17 +0700

    resolve width

--- index.php
+++ index.php
@@ -20,8 +20,8 @@
     <noscript><link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/ionicons.min.css?ver=0.0.2' ?>"></noscript>
     <link rel="preload" media="screen" href="<?= get_template_directory_uri() . '/css/main.css' ?>" as="style" onload="this.onload=null;this.rel='stylesheet'">
     <noscript><link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/main.css?ver=0.0.2' ?>"></noscript>
-    <!-- <link rel="preload" media="screen" href="<?php // get_template_directory_uri() . '/style.css' ?>" as="style" onload="this.onload=null;this.rel='stylesheet'">
-    <noscript><link rel="stylesheet" href="<?php // get_template_directory_uri() . '/style.css?ver=0.0.1' ?>"></noscript> -->
+    <link rel="preload" media="screen" href="<?= get_template_directory_uri() . '/style.css' ?>" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript><link rel="stylesheet" href="<?= get_template_directory_uri() . '/style.css?ver=0.0.1' ?>"></noscript>
 
     <?php wp_head(); ?>
     

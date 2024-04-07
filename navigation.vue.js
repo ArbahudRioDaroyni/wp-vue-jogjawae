cccb8f95000899c8f73831ccbe6e87b254da3107
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-07 15:17:20 +0700

    add navbar

--- navigation.vue.js
+++ navigation.vue.js
@@ -2,44 +2,80 @@ const Navigation = {
   name: 'Navigation',
   template: /*html*/`
     <header>
-      <!-- START NAV -->
-      <nav class="navbar">
-        <div class="container">
-          <div class="navbar-bars">
-            <a href="#" class="navbar-title sidebar-toggle" style="padding: 0;"><i class="ion-navicon-round"></i></a>
-            <a href="index.html" class="navbar-title">Bali Travel Time</a>
+      <!-- Start Nav -->
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="https://bulma.io">
+            <img src="https://jogjawae.com/wp-content/uploads/2021/01/cropped-Favicon-Jogjawae.png" alt="Jogjawae">
+          </a>
+
+          <a role="button" class="navbar-burger" @click="toggleNavbar" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div :class="['navbar-menu', isOpen ? 'is-active' : '']" id="navbarBasicExample">
+          <div class="navbar-start">
+            <a class="navbar-item">
+              Home
+            </a>
+
+            <a class="navbar-item">
+              Documentation
+            </a>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                More
+              </a>
+
+              <div class="navbar-dropdown">
+                <a class="navbar-item">
+                  About
+                </a>
+                <a class="navbar-item is-selected">
+                  Jobs
+                </a>
+                <a class="navbar-item">
+                  Contact
+                </a>
+                <hr class="navbar-divider">
+                <a class="navbar-item">
+                  Report an issue
+                </a>
+              </div>
+            </div>
           </div>
-          <div class="navbar-item">
-            <!-- <img src="https://jogjawae.com/wp-content/uploads/2021/01/cropped-Favicon-Jogjawae.png" alt="Jogjawae"> -->
-            <a href="index.html" class="navbar-title">Jogja Wae</a>
-            <ul>
-              <li><a href="destination.html">Destination</a></li>
-              <li><a href="gallery.html">Gallery</a></li>
-              <li><a data-slide="slides" data-slide-target="#discover">Discover</a></li>
-              <li><a href="news.html"> News</a></li>
-              <li><button class="btn-login" id="openLogin">LOGIN</button></li>
-            </ul>
+
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
+                <a class="button is-primary">
+                  <strong>Sign up</strong>
+                </a>
+                <a class="button is-light">
+                  Log in
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </nav>
-      <!-- END NAV -->
-      <!-- Sidebar -->
-      <div class="sidebar">
-      <ul class="sidebar-list">
-        <li><a href="" class="close"><span class="ion-android-close"></span></a></li>
-        <li class="sidebar-list-hover"><a href="index.html">Home</a></li>
-          <li class="sidebar-list-hover"><a href="destination.html">Destination</a></li>
-          <li class="sidebar-list-hover"><a href="gallery.html">Gallery</a></li>
-          <li class="sidebar-list-hover"><a href="index.html#discover">Discover</a></li>
-          <li class="sidebar-list-hover"><a href="news.html"> News</a></li>
-          <li><a class="btn btn-orange btn-round" href="login.html"> Login</a></li>
-      </ul>                    
-      </div>
-
-      <!-- Sidebar Overlay -->
-      <section class="sidebar-overlay"></section>
     </header>
-  `
+  `,
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  methods: {
+    toggleNavbar() {
+      this.isOpen = !this.isOpen;
+    }
+  }
 }
 
 export default Navigation

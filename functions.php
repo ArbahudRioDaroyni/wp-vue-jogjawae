9bb39937d1388c532111716cce376ed66f868b5f
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-09 18:56:18 +0700

    delete css wp

--- functions.php
+++ functions.php
@@ -1,20 +1,18 @@
 <?php
 function wpassist_remove_block_library_css(){
     wp_dequeue_style( 'wp-block-library' );
-    wp_dequeue_style( 'global-styles-inline-css' );
-    wp_dequeue_style( 'classic-theme-styles-inline-css' );
 } 
 add_action( 'wp_enqueue_scripts', 'wpassist_remove_block_library_css' );
 
 // Function to remove unnecessary stylesheets
-// function remove_unnecessary_styles() {
-//     // Check if it's the front-end
-//     if (!is_admin()) {
-//         // Remove global-styles-inline-css
-//         wp_deregister_style('global-styles-inline-css');
+function remove_unnecessary_styles() {
+    // Check if it's the front-end
+    if (!is_admin()) {
+        // Remove global-styles-inline-css
+        wp_deregister_style('global-styles-inline-css');
         
-//         // Remove classic-theme-styles-inline-css
-//         wp_deregister_style('classic-theme-styles-inline-css');
-//     }
-// }
-// add_action('wp_enqueue_scripts', 'remove_unnecessary_styles', 100);
+        // Remove classic-theme-styles-inline-css
+        wp_deregister_style('classic-theme-styles-inline-css');
+    }
+}
+add_action('wp_enqueue_scripts', 'remove_unnecessary_styles', 100);

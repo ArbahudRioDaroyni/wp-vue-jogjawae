571e5b0448eb4fe229498b3c6381328a0e0b32a9
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-09 19:03:50 +0700

    fix problem

--- category.component.js
+++ category.component.js
@@ -7,7 +7,7 @@ const Category = {
           <div class="column is-12 is-7-fullhd is-8-desktop has-text-centered">
             <span class="has-text-grey-dark">Lorem ipsum</span>
             <h2 class="mt-2 mb-4 is-size-1 is-size-3-mobile has-text-weight-bold">{{ $route.params.category.charAt(0).toUpperCase() + $route.params.category.slice(1) }}</h2>
-            <p class="subtitle has-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan aliquet orci.</p>
+            <p class="subtitle has-text-grey">Dapatkan informasi wisata {{ $route.params.category.charAt(0).toUpperCase() + $route.params.category.slice(1) }} yang menarik di {{ window.location.hostname }}.</p>
           </div>
         </div>
         <article class="columns is-multiline">
@@ -21,7 +21,7 @@ const Category = {
           </div>
         </article>
         <div v-if="showLoadMoreButton" class="has-text-centered">
-          <button @click="loadMorePosts" class="button is-primary">Load More</button>
+          <button @click="loadMorePosts" class="button is-primary">Muat lainnya</button>
         </div>
         <div v-if="loading" class="has-text-centered mt-4">
           <i class="fas fa-spinner fa-spin"></i> Loading...

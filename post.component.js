30f1c8cee8f0f0779d8fdae46fe7206c561265de
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-12 15:45:01 +0700

    track recursive loop

--- post.component.js
+++ post.component.js
@@ -104,30 +104,28 @@ const Post = {
     }
   },
   created() {
-    const slug = this.$route.params.slug;
-    const isValidSlug = /^[a-zA-Z0-9-]+$/.test(slug);
-
-    if (isValidSlug) {
-      console.warn('Slug valid:', slug);
+    this.watchSlug();
+  },
+  methods: {
+    watchSlug() {
       this.$watch(
         function() {
           return this.$route.params.slug;
         },
         function() {
-          if (this.$route.params.slug !== undefined && this.$route.params.slug !== null) {
+          const isValidSlug = /^[a-zA-Z0-9-]+$/.test(this.$route.params.slug);
+          if (isValidSlug) {
             this.loading = true;
             this.fetchDataPost();
             this.fetchCategories();
             this.fetchLatestPosts();
+          } else {
+            this.$router.push({ path: '/' });
           }
         },
         { immediate: true }
       )
-    } else {
-      this.$router.push({ path: '/' });
-    }
-  },
-  methods: {
+    },
     async fetchDataPost() {
       this.scrollToTop();
       this.error = this.post = null
@@ -135,7 +133,7 @@ const Post = {
       // const url = `${API_URL}${this.$route.params.slug}`
       // this.post = await (await fetch(url)).json()
       try {
-        const url = `https://jogjawae.com/wp-json/wp/v2/posts?slug=${this.$route.params.slug}`;
+        const url = `${window.location.origin}/wp-json/wp/v2/posts?slug=${this.$route.params.slug}`;
         const postData = await (await fetch(url)).json();
         
         // Memformat tanggal modified_gmt
@@ -158,7 +156,7 @@ const Post = {
     },
     async fetchCategories() {
       try {
-        const url = 'https://jogjawae.com/wp-json/wp/v2/categories';
+        const url = '${window.location.origin}/wp-json/wp/v2/categories';
         const response = await fetch(url);
         if (!response.ok) {
           throw new Error('Failed to fetch categories');
@@ -173,7 +171,7 @@ const Post = {
     },
     async fetchLatestPosts() {
       try {
-        const response = await fetch(`https://jogjawae.com/wp-json/wp/v2/posts?per_page=3&_embed`);
+        const response = await fetch(`${window.location.origin}/wp-json/wp/v2/posts?per_page=3&_embed`);
         if (response.ok) {
           this.latestposts = await response.json();
         } else {
@@ -222,7 +220,7 @@ const Post = {
     // async fetchRelatedPosts() {
       // if (this.post && this.post[0] && this.post[0].tags && this.post[0].tags.length > 0) {
       //   const tagIds = this.post[0].tags.map(tag => tag.id).join(',');
-      //   const response = await fetch(`https://jogjawae.com/wp-json/wp/v2/posts?tags=${tagIds}&per_page=3`);
+      //   const response = await fetch(`${window.location.origin}/wp-json/wp/v2/posts?tags=${tagIds}&per_page=3`);
         // if (response.ok) {
         // if (this.post) {
           // this.relatedPosts = await response.json();

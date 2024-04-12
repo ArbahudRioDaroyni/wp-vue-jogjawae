b621c1c0ebf7e84bc71c3b4e0d0b2c2711cd289f
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-12 14:13:03 +0700

    indent level headings

--- table-of-contents.vue.js
+++ table-of-contents.vue.js
@@ -9,29 +9,6 @@ const TableOfContents = {
             <a :href="'#' + heading.id" @click="scrollToHeading(heading)">
               {{ heading.title }}
             </a>
-            <ul>
-              <div v-for="subHeading in headings" :key="subHeading.id">
-                <div v-if="subHeading.parentId === heading.id && subHeading.level === 2">
-                  <li>
-                    <a :href="'#' + subHeading.id" @click="scrollToHeading(subHeading)">
-                      {{ subHeading.title }}
-                    </a>
-                    <ul>
-                      <div v-for="subSubHeading in headings" :key="subSubHeading.id">
-                        <div v-if="subSubHeading.parentId === subHeading.id && subSubHeading.level === 3">
-                          <li>
-                            <a :href="'#' + subSubHeading.id" @click="scrollToHeading(subSubHeading)">
-                              {{ subSubHeading.title }}
-                            </a>
-                            <!-- Repeat for subSubSubHeading, subSubSubSubHeading, etc. -->
-                          </li>
-                        </div>
-                      </div>
-                    </ul>
-                  </li>
-                </div>
-              </div>
-            </ul>
           </li>
         </div>
       </div>

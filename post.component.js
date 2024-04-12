dbc9467f66c2556e7abcd5e8c2767ff67920d0f4
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-12 12:54:25 +0700

    track var headings

--- post.component.js
+++ post.component.js
@@ -126,12 +126,13 @@ const Post = {
     } else {
       this.$router.push({ path: '/' });
     }
-
+    
     const headings = Array.from(document.querySelectorAll(".content-article h1, .content-article h2, .content-article h3, .content-article h4, .content-article h5, .content-article h6"));
     this.headings = headings.map(heading => ({
       id: heading.id,
       title: heading.textContent
     }));
+    console.log(this.headings);
   },
   methods: {
     async fetchDataPost() {
@@ -209,7 +210,11 @@ const Post = {
       // }
     // }
   },
-  mounted() {}
+  mounted() {
+    // Table 0f Contents
+    // Temukan semua elemen heading di dalam konten dan buat array headings
+    
+  }
 }
 
 export default Post

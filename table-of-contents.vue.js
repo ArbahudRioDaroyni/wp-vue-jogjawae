3c4774af0064ad0b9f7fb33222bfcf082d537154
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-12 13:34:46 +0700

    indent level headings

--- table-of-contents.vue.js
+++ table-of-contents.vue.js
@@ -3,38 +3,38 @@ const TableOfContents = {
   template: `
   <div class="table-of-contents">
     <ul>
-      <template v-for="heading in headings" :key="heading.id">
-        <template v-if="heading.level === 1">
+      <div v-for="heading in headings" :key="heading.id">
+        <div v-if="heading.level === 1">
           <li>
             <a :href="'#' + heading.id" @click="scrollToHeading(heading)">
               {{ heading.title }}
             </a>
             <ul>
-              <template v-for="subHeading in headings" :key="subHeading.id">
-                <template v-if="subHeading.parentId === heading.id && subHeading.level === 2">
+              <div v-for="subHeading in headings" :key="subHeading.id">
+                <div v-if="subHeading.parentId === heading.id && subHeading.level === 2">
                   <li>
                     <a :href="'#' + subHeading.id" @click="scrollToHeading(subHeading)">
                       {{ subHeading.title }}
                     </a>
                     <ul>
-                      <template v-for="subSubHeading in headings" :key="subSubHeading.id">
-                        <template v-if="subSubHeading.parentId === subHeading.id && subSubHeading.level === 3">
+                      <div v-for="subSubHeading in headings" :key="subSubHeading.id">
+                        <div v-if="subSubHeading.parentId === subHeading.id && subSubHeading.level === 3">
                           <li>
                             <a :href="'#' + subSubHeading.id" @click="scrollToHeading(subSubHeading)">
                               {{ subSubHeading.title }}
                             </a>
                             <!-- Repeat for subSubSubHeading, subSubSubSubHeading, etc. -->
                           </li>
-                        </template>
-                      </template>
+                        </div>
+                      </div>
                     </ul>
                   </li>
-                </template>
-              </template>
+                </div>
+              </div>
             </ul>
           </li>
-        </template>
-      </template>
+        </div>
+      </div>
     </ul>
   </div>
   `,

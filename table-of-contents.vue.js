236a66bd9f48b4c9f13b6ab2553674f76f198328
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-12 16:27:59 +0700

    add index in array headings toc

--- table-of-contents.vue.js
+++ table-of-contents.vue.js
@@ -2,26 +2,24 @@ const TableOfContents = {
   name: 'TableOfContents',
   template: `
   <div>
-    <ul>
-      <template v-for="(heading, index) in headings" :key="heading.id">
-        <template v-if="index === 0 || heading.level > headings[index - 1].level">
-          <ul>
-            <li>
-              <a :href="'#' + heading.id" @click="scrollToHeading(heading)">
-                {{ heading.title }}
-              </a>
-            </li>
-          </ul>
-        </template>
-        <template v-else>
+    <template v-for="(heading, index) in headings" :key="heading.id">
+      <template v-if="index === 0 || heading.level > headings[index - 1].level">
+        <ul>
           <li>
             <a :href="'#' + heading.id" @click="scrollToHeading(heading)">
               {{ heading.title }}
             </a>
           </li>
-        </template>
+        </ul>
       </template>
-    </ul>
+      <template v-else>
+        <li>
+          <a :href="'#' + heading.id" @click="scrollToHeading(heading)">
+            {{ heading.title }}
+          </a>
+        </li>
+      </template>
+    </template>
   </div>
   `,
   props: {

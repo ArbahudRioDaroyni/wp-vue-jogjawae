0fde89364fb67cddf1421da385e5e444d684c3be
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-12 13:32:58 +0700

    indent level headings

--- post.component.js
+++ post.component.js
@@ -189,18 +189,30 @@ const Post = {
         behavior: 'smooth'
       });
     },
-    getTableofContents() {
-      // get ell h1-h6 in .content-article => store as array
-      const headings = Array.from(document.querySelectorAll(".content-article h1, .content-article h2, .content-article h3, .content-article h4, .content-article h5, .content-article h6"));
-      this.headings = headings.map(heading => {
-        const id = heading.textContent.trim().replace(/\s+/g, '-'); // Membuat id dari judul dengan menghapus spasi dan mengganti dengan tanda '-' (strip)
-        heading.id = id; // Tambahkan id ke elemen heading
-        return {
-          id: id,
-          title: heading.textContent
-        };
-      });
-    }
+
+    // Start Table 0f Contents
+    createTableofContents() {
+    // get all h1-h6 elements in .content-article => store as array
+    const headings = Array.from(document.querySelectorAll(".content-article h1, .content-article h2, .content-article h3, .content-article h4, .content-article h5, .content-article h6"));
+    this.headings = headings.map(heading => {
+      const id = heading.textContent.trim().replace(/\s+/g, '-'); // Create an id from the title by removing spaces and replacing with '-' (dash)
+      heading.id = id; // Add id to the heading element
+      return {
+        id: id,
+        title: heading.textContent,
+        level: parseInt(heading.tagName.substring(1)) // Get the heading level (e.g., h1, h2, etc.)
+      };
+    });
+
+    // add attr id to all h1-h6 elements in .article-content
+    const articleContentHeadings = Array.from(document.querySelectorAll(".article-content h1, .article-content h2, .article-content h3, .article-content h4, .article-content h5, .article-content h6"));
+    articleContentHeadings.forEach(heading => {
+      const id = heading.textContent.trim().replace(/\s+/g, '-'); // Create an id from the title by removing spaces and replacing with '-' (dash)
+      heading.id = id; // Add id to the heading element
+    });
+  }
+    // Start Table 0f Contents
+
     // async fetchRelatedPosts() {
       // if (this.post && this.post[0] && this.post[0].tags && this.post[0].tags.length > 0) {
       //   const tagIds = this.post[0].tags.map(tag => tag.id).join(',');
@@ -217,13 +229,7 @@ const Post = {
   },
   updated() {
     // Start Table 0f Contents
-    this.getTableofContents();
-    // add attr id to ell h1-h6 in .article-content
-    const articleContentHeadings = Array.from(document.querySelectorAll(".article-content h1, .article-content h2, .article-content h3, .article-content h4, .article-content h5, .article-content h6"));
-    articleContentHeadings.forEach(heading => {
-      const id = heading.textContent.trim().replace(/\s+/g, '-'); // Membuat id dari judul dengan menghapus spasi dan mengganti dengan tanda '-' (strip)
-      heading.id = id; // Tambahkan id ke elemen heading
-    });
+    this.createTableofContents();
     // End Table 0f Contents
   }
 }
--- table-of-contents.vue.js
+++ table-of-contents.vue.js
@@ -3,11 +3,38 @@ const TableOfContents = {
   template: `
   <div class="table-of-contents">
     <ul>
-      <li v-for="heading in headings" :key="heading.id">
-        <a :href="'#' + heading.id" @click="scrollToHeading(heading)">
-          {{ heading.title }}
-        </a>
-      </li>
+      <template v-for="heading in headings" :key="heading.id">
+        <template v-if="heading.level === 1">
+          <li>
+            <a :href="'#' + heading.id" @click="scrollToHeading(heading)">
+              {{ heading.title }}
+            </a>
+            <ul>
+              <template v-for="subHeading in headings" :key="subHeading.id">
+                <template v-if="subHeading.parentId === heading.id && subHeading.level === 2">
+                  <li>
+                    <a :href="'#' + subHeading.id" @click="scrollToHeading(subHeading)">
+                      {{ subHeading.title }}
+                    </a>
+                    <ul>
+                      <template v-for="subSubHeading in headings" :key="subSubHeading.id">
+                        <template v-if="subSubHeading.parentId === subHeading.id && subSubHeading.level === 3">
+                          <li>
+                            <a :href="'#' + subSubHeading.id" @click="scrollToHeading(subSubHeading)">
+                              {{ subSubHeading.title }}
+                            </a>
+                            <!-- Repeat for subSubSubHeading, subSubSubSubHeading, etc. -->
+                          </li>
+                        </template>
+                      </template>
+                    </ul>
+                  </li>
+                </template>
+              </template>
+            </ul>
+          </li>
+        </template>
+      </template>
     </ul>
   </div>
   `,

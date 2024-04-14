2030672b2ecaced01c828cef31b7fe5024fd2883
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-14 11:20:21 +0700

    fixing table of contents

--- table-of-contents.vue.js
+++ table-of-contents.vue.js
@@ -2,24 +2,31 @@ const TableOfContents = {
   name: 'TableOfContents',
   template: `
   <div>
-    <template v-for="(heading, index) in headings" :key="heading.id">
-      <template v-if="index === 0 || heading.level > headings[index - 1].level">
-        <ul>
+    <ul>
+      <template v-for="(heading, index) in headings" :key="heading.id">
+        <template v-if="index === 0 || heading.level === headings[index - 1].level">
           <li>
-            <a :href="'#' + heading.id" @click="scrollToHeading(heading)">
-              {{ heading.title }}
-            </a>
+            <a :href="'#' + heading.id" @click="scrollToHeading(heading)">{{ heading.title }}</a>
           </li>
-        </ul>
+        </template>
+        <template v-else-if="heading.level > headings[index - 1].level">
+          <template v-for="let i = 0; i < heading.level - headings[index - 1].level; i++">
+            <ul>
+          </template>
+          <li>
+            <a :href="'#' + heading.id" @click="scrollToHeading(heading)">{{ heading.title }}</a>
+          </li>
+        </template>
+        <template v-else>
+          <template v-for="let i = 0; i < headings[index - 1].level - heading.level; i++">
+            </ul>
+          </template>
+          <li>
+            <a :href="'#' + heading.id" @click="scrollToHeading(heading)">{{ heading.title }}</a>
+          </li>
+        </template>
       </template>
-      <template v-else>
-        <li>
-          <a :href="'#' + heading.id" @click="scrollToHeading(heading)">
-            {{ heading.title }}
-          </a>
-        </li>
-      </template>
-    </template>
+    </ul>
   </div>
   `,
   props: {

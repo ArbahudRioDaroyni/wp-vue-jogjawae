7d85ab1b0cee22fe81b3cad56208f3fbfcf5cc56
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-15 12:33:37 +0700

    fixing structure file

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ const { createApp } = Vue
 
 import router from './router.js'
 import Navigation from './navigation.vue.js'
-import Home from './home.component.js'
 import Post from './post.component.js'
-// import Category from './category.component.js'
 import ListArticle from './list-article.vue.js'
 import TableOfContents from './table-of-contents.vue.js'
 import Footer from './footer.vue.js'
@@ -17,9 +15,7 @@ export const app = createApp({
 
 app.use(router)
 app.component('Navigation', Navigation)
-  .component('Home', Home)
   .component('Post', Post)
-  // .component('Category', Category)
   .component('ListArticle', ListArticle)
   .component('TableOfContents', TableOfContents)
   .component('Footer', Footer)

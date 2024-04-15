aa1c838a434494d801467514bc41e8eb4be18bc5
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-15 10:14:28 +0700

    spare card list article

--- category.component.js
+++ category.component.js
@@ -14,56 +14,15 @@ const Category = {
     <section class="section pt-0">
       <div class="container">
         <div class="columns is-multiline">
-          <article
+        <!-- card.vue.js -->
+          <ListArticle
             v-for="(post, index) in posts"
             :key="post.id"
             :class="index === 0 ? 'column is-8 mb-5' : 'column is-4 mb-5'"
-            @click="goToLink(post.slug)">
-            <div class="card">
-              <div class="card-image">
-                <figure class="image is-6by3">
-                  <img
-                    :src="post.yoast_head_json.og_image[0].url"
-                    :alt="post.title.rendered"
-                    loading="lazy"
-                    decoding="async"
-                    class="fit-cover"
-                    :style="index === 0 ? 'max-height: 350px' : 'max-height: 350px'"
-                  />
-                </figure>
-              </div>
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-left">
-                    <figure class="image is-48x48">
-                      <img
-                        src="post.yoast_head_json.schema.@graph[last].image.contentUrl"
-                        :alt="post.yoast_head_json.author"
-                        loading="lazy"
-                        decoding="async"
-                      />
-                    </figure>
-                  </div>
-                  <div class="media-content" style="overflow: unset;">
-                    <h2
-                      v-html="post.title.rendered"
-                      class="title is-4">
-                    </h2>
-                    <p class="subtitle is-6">{{ post.yoast_head_json.author }}.</p>
-                  </div>
-                </div>
-                <div class="content">
-                  {{ truncateText(post.excerpt.rendered, 200) }} <a>@bulmaio</a>. <a href="#">#css</a>
-                  <a href="#">#responsive</a>
-                  <br />
-                  <time
-                    :datetime="post.modified_gmt"
-                    v-html="post.yoast_head_json.article_published_time">
-                  </time>
-                </div>
-              </div>
-            </div>
-          </article>
+            :post="post"
+            @click="goToLink(post.slug)"
+          />
+        <!-- card.vue.js -->
         </div>
         <div
           v-if="showLoadMoreButton"
---
+++ list-article.vue.js
@@ -0,0 +1,59 @@
+const TableOfContents = {
+  name: 'ListArticle',
+  template: `
+    <article :post="post">
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-6by3">
+            <img
+              :src="post.yoast_head_json.og_image[0].url"
+              :alt="post.title.rendered"
+              loading="lazy"
+              decoding="async"
+              class="fit-cover"
+              :style="index === 0 ? 'max-height: 350px' : 'max-height: 350px'"
+            />
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-48x48">
+                <img
+                  src="post.yoast_head_json.schema.@graph[last].image.contentUrl"
+                  :alt="post.yoast_head_json.author"
+                  loading="lazy"
+                  decoding="async"
+                />
+              </figure>
+            </div>
+            <div class="media-content" style="overflow: unset;">
+              <h2
+                v-html="post.title.rendered"
+                class="title is-4">
+              </h2>
+              <p class="subtitle is-6">{{ post.yoast_head_json.author }}.</p>
+            </div>
+          </div>
+          <div class="content">
+            {{ truncateText(post.excerpt.rendered, 200) }} <a>@bulmaio</a>. <a href="#">#css</a>
+            <a href="#">#responsive</a>
+            <br />
+            <time
+              :datetime="post.modified_gmt"
+              v-html="post.yoast_head_json.article_published_time">
+            </time>
+          </div>
+        </div>
+      </div>
+    </article>
+  `,
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+}
+
+export default ListArticle

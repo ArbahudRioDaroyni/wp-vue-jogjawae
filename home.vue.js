d15db308b14e5e6405d91f16831d911a422acd0d
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-15 12:32:38 +0700

    fixing structure file

--- home.vue.js
+++ home.vue.js
@@ -1,9 +1,44 @@
 const Home = {
+  name: 'Home',
   template: `
     <Navigation />
-    <Home />
+    <main class="section mt-6">
+      <section class="container">
+        <div class="columns is-multiline">
+          <!-- list-article.vue.js -->
+            <ListArticle :posts="posts" />
+          <!-- list-article.vue.js -->
+        </div>
+      </section>
+    </main>
     <Footer />
-  `
+  `,
+  data() {
+    return {
+      loading: false,
+      posts: null,
+      error: null,
+    }
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      this.error = this.posts = null
+      this.loading = true
+      try {
+        const API_field = "id,modified_gmt,slug,title,excerpt,yoast_head_json.author,yoast_head_json.og_image";
+        const response = await fetch(`${window.location.origin}/wp-json/wp/v2/posts?_fields=${API_field}&per_page=8`);
+        this.posts = await response.json();
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        this.error = 'Error fetching data.';
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
 }
 
 export default Home

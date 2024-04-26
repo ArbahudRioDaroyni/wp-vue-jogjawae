c59c3fb674383d8595abc790d4a84a01c91d9ddc
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-26 09:29:19 +0700

    add pos-thumbnails

--- functions.php
+++ functions.php
@@ -6,3 +6,5 @@ add_action( 'wp_enqueue_scripts', 'wpassist_remove_block_library_css' );
 
 remove_action( 'wp_enqueue_scripts', 'wp_enqueue_global_styles' );
 remove_action( 'wp_footer', 'wp_enqueue_global_styles', 1 );
+
+add_theme_support( 'post-thumbnails' );

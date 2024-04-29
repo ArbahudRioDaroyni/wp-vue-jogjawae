29aa1c2a1599355b5862cee8096b16477f7079c9
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-04-29 22:04:21 +0700

    create me page

--- footer.vue.js
+++ footer.vue.js
@@ -12,7 +12,7 @@ const Footer = {
           <div>
             <ul class="is-flex is-flex-wrap-wrap is-align-items-center is-justify-content-center">
               <li class="mr-4">
-                <router-link :to="'/' + me" class="button is-white">Me</router-link>
+                <router-link :to="'/' + 'me'" class="button is-white">Me</router-link>
               </li>
               <li class="mr-4"><a class="button is-white" href="#">Company</a></li>
               <li class="mr-4"><a class="button is-white" href="#">Services</a></li>
